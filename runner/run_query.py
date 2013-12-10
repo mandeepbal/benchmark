5b1ae532dc9cb5bef9801fdb5c4fd07e53981630
Ahir Reddy <ahirreddy@gmail.com>
2013-12-09 16:58:54 -0800

    changed concurrent map from thread based to process based

--- runner/run_query.py
+++ runner/run_query.py
@@ -13,7 +13,7 @@ import os
 import time
 import datetime
 import re
-import threading
+from multiprocessing import Pool
 from StringIO import StringIO
 from pg8000 import DBAPI
 
@@ -662,28 +662,7 @@ def main():
   outfile.close()
 
 def concurrent_map(func, data):
-    """
-    Similar to the bultin function map(). But spawn a thread for each argument
-    and apply `func` concurrently.
-
-    Note: unlike map(), we cannot take an iterable argument. `data` should be an
-    indexable sequence.
-    """
-
-    N = len(data)
-    result = [None] * N
-
-    # wrapper to dispose the result in the right slot
-    def task_wrapper(i):
-        result[i] = func(data[i])
-
-    threads = [threading.Thread(target=task_wrapper, args=(i,)) for i in xrange(N)]
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
-
-    return result
+  return Pool().map(func, data)
 
 if __name__ == "__main__":
   main()
