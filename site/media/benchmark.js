9e2c29540d7fb55b6614d534b41833286d3e5f43
Ahir Reddy <ahirreddy@gmail.com>
2014-01-30 16:55:03 -0800

    click graph for alternate data

--- site/media/benchmark.js
+++ site/media/benchmark.js
@@ -16,6 +16,7 @@
     });
 
 function make_graph(data_in, labels) {
+  alt_data = [[10],[10],[10],[10],[10],[10],[10],[10],[10]]
   var max_value = 0;
   for (var i=0; i < data_in.length; i++) {
     var array_max = Math.max.apply(Math, data_in[i]); // really?
@@ -34,7 +35,8 @@ function make_graph(data_in, labels) {
       .width(w)
       .height(h)
       .margin(20)
-      .left(30);
+      .left(30)
+      .cursor("pointer");
   var y = pv.Scale.linear(0, max_value).range(0, h);
 
   vis.add(pv.Rule)
@@ -52,18 +54,37 @@ function make_graph(data_in, labels) {
     .data(data_in)
     .left(function() { return this.index * (total_bar_width); })
 
-  var idx = -1;
-  chartPanel.add(pv.Bar)
-    .data(function(i) { return i; })
-    .width(bar_width)
-    .height(y)
-    .bottom(0)
-    .left(function() { return this.index * group_spacing ; })
-  .anchor("top").add(pv.Label)
-    .textAlign(function(d) { return (d < (4.0/5.0)*max_value) ? "left" : "right"; } )
-    .textBaseline("middle")
-    .textAngle(-Math.PI / 2)
-    .textDecoration(function(d) { return (d == 0) ? ["line-through"] : []; })
-    .text(function(d) { idx++; return (d == 0) ? "" : labels[idx]; });
+  function drawChart(data, labels) {
+    chartPanel.data(data);
+    var idx = -1;
+    chartPanel.add(pv.Bar)
+      .data(function(i) { return i; })
+      .width(bar_width)
+      .height(y)
+      .bottom(0)
+      .left(function() { return this.index * group_spacing ; })
+    .anchor("top").add(pv.Label)
+      .textAlign(function(d) { return (d < (4.0/5.0)*max_value) ? "left" : "right"; } )
+      .textBaseline("middle")
+      .textAngle(-Math.PI / 2)
+      .textDecoration(function(d) { return (d == 0) ? ["line-through"] : []; })
+      .text(function(d) { idx++; return (d == 0) ? "" : labels[idx]; });
+    vis.render();
+  }
 
-  vis.render();}
+  var clicked = false;
+  function onClick () {
+    if (clicked) {
+      clicked = false;
+      drawChart(alt_data, labels);
+    } else {
+      clicked = true;
+      drawChart(data_in, labels);
+    }
+  }
+
+  vis.event("click", onClick)
+
+  drawChart(data_in, labels);
+}
+
